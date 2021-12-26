0d05d2e96c92aecff671e8dbe9b96b0c686a3231
PPM5 Admin <autoinst578@gmail.com>
2021-12-26 10:22:48 +0700

    convert double to string

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string a = "Hello World!";
+            double a;
+            a = double.Parse(Console.ReadLine());
             Console.WriteLine(a);
         }
     }
