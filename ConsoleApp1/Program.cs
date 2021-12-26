17f68a9fe235fb3e066dd871aa584c0787fa899d
PPM5 Admin <autoinst578@gmail.com>
2021-12-26 10:20:21 +0700

    string

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string a;
-            a = Console.ReadLine();
+            string a = "Hello World!";
+            Console.WriteLine(a);
         }
     }
 }
