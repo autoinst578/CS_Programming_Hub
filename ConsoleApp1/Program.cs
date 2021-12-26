6a71b8581a539304411c95ced60fc7d8c35eefe6
PPM5 Admin <autoinst578@gmail.com>
2021-12-26 11:33:32 +0700

    assign the value to array

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,8 +10,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int elements = 100;
-            int[] numbers = new int[elements];
+            int elements = 5;
+            double[] numbers = new double[elements];
+            numbers[0] = 10;
+            numbers[1] = 25;
+            numbers[2] = 27;
+            for (int i =0; i < elements; i++)
+            {
+                Console.WriteLine(numbers[i]);
             }
         }
     }
