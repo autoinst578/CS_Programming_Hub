e45b02ceef3cede0942ac6999973a13a71ecdf03
PPM5 Admin <autoinst578@gmail.com>
2021-12-26 09:43:09 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char x = 'a';
+            int i = (int)x;
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
